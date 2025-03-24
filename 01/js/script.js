2a61895389846925722598dee4c35c54f3a20b81
Adrian Esppen <adrian.esppen@educa.jcyl.es>
2025-03-24 09:22:54 +0100

    add script ejer1

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,2 +1,15 @@
+var titulo = document.getElementById("titulo")
+var parrafo = document.getElementsByClassName("parrafo")
+var cuerpo = document.getElementById("cuerpo")
 
+titulo.innerText = "Nuevo titulo"
+parrafo[0].innerText = "parrafo 1 modificado"
+parrafo[1].innerText = "parrafo 2 modificado"
+parrafo[2].innerText = "parrafo 3 modificado"
 
+parrafo[0].outerHTML = "<div>Parrafo 11 modificado</div>"
+
+cuerpo.style.backgroundColor = "#f4f4f4"
+cuerpo.style.textAlign = "center"
+
+titulo.style.color = "#333"
