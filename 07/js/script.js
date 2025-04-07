b9d3fce95c38aab50bc98b616c964f97c91bef7e
Marcos Fernández <marcos.fergar@educa.jcyl.es>
2025-04-07 08:49:30 +0200

    12

--- 07/js/script.js
+++ 07/js/script.js
@@ -1,8 +1,9 @@
 var contenedor = document.querySelector("#contenedor")
 
 document.getElementById("agregarFinal").addEventListener("click", function(){
+    
     // Crear un nuevo párrafo
-   let parrafo = document.createElement("p")
+    let parrafo = document.createElement("p")
     //añadir texto al párrafo
     parrafo.innerText = document.getElementById("texto").value
     //añadir el párrafo al contenedor
@@ -10,6 +11,7 @@ document.getElementById("agregarFinal").addEventListener("click", function(){
     //agregar el color de texto del nuevo parrafo sea rojo
     parrafo.style.color = "red";
 });
+
 //agregar párrafo al principio
 document.getElementById("agregarPpio").addEventListener("click", function(){
     // Crear un nuevo párrafo
@@ -21,6 +23,7 @@ document.getElementById("agregarPpio").addEventListener("click", function(){
     //agregar estilos al párrafo texto azul
     parrafo.style.color = "blue";
 });
+
 //agregar párrafo en una posición concreta
 document.getElementById("agregarPosicion").addEventListener("click", function(){
     // Crear un nuevo párrafo
@@ -28,19 +31,18 @@ document.getElementById("agregarPosicion").addEventListener("click", function(){
     //añadir texto al párrafo
     parrafo.innerText = document.getElementById("texto").value
     //añadir el párrafo al contenedor inserBefore
-    contenedor.insertBefore(parrafo)
+    contenedor.insertBefore(parrafo,contenedor.firstChild)
     //agregar estilos color del texto verde
-    parrafo.style.color = "green";
+     parrafo.style.color = "green";
 });
+
 //eliminar el primer párrafo
 document.getElementById("eliminar").addEventListener("click", function(){
     //eliminar el primer párrafo
-    contenedor.removeChild(contenedor.children[0])
-
+    contenedor.removeChild(contenedor.firstChild)
 });
+
 document.getElementById("eliminarTodos").addEventListener("click", function(){
     //eliminar todos los párrafos
-    while (contenedor.firstChild) {
-        contenedor.removeChild(contenedor.firstChild);
-    }
+    contenedor.innerHTML = ""
 });
